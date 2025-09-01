67e2a70f796807b0f7f5e69a6f4ef4463f983ff2
Juan David <juandaguti@icloud.com>
2025-09-01 17:14:21 +0100

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Juan David Gutierrez Olarte on 1/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
